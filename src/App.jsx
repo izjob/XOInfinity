b79e7928582954b573d7f1b7162bcd8723699128
izjob <izian.job@gmail.com>
2025-04-03 11:12:14 +0200

    cambios de estilo, tablero y cambio de turno

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,54 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const TURNS = {
+  x: 'X',
+  o: 'O'
+}
 
+//para borrar guardas posiciones en listas ,borras la primera y añades la nueva
+
+
+const Square = ({children,updateBoard, index}) =>{
+  return(
+    <div className='square' onClick={updateBoard}>
+      {children}
+    </div>
+  )
+}
+function App() {
+  const [board,setBoard] = useState(Array(9).fill(null))
+  
+  const [turn,setTurn] = useState(TURNS.o)
+
+  const updateBoard = () =>{
+    const newTurn = turn===TURNS.x ? TURNS.o : TURNS.x
+    setTurn(newTurn)
+  }
   return (
-    <>
+    <main className='board'>
       <h1>XOInfinity</h1>
-    </>
+      <section className='game'>
+        {
+            board.map((_,index) =>{
+              return (
+                <Square 
+                  key={index}
+                  index={index}
+                  updateBoard={updateBoard}
+                >
+                  <span>{index}</span>
+                </Square>
+              )
+            })
+        }
+        
+      </section>
+      <h2>turn:</h2>
+      <section className='turn'>
+        <Square>{turn}</Square>
+      </section>
+    </main>
   )
 }
 
