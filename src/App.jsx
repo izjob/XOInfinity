a281c4c9ee7c754acd92c5f50d3f58d5eb8a1461
izjob <izian.job@gmail.com>
2025-04-03 13:15:15 +0200

    juego original terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { use, useState } from 'react'
 import './App.css'
 
 const TURNS = {
@@ -7,6 +7,9 @@ const TURNS = {
 }
 
 //para borrar guardas posiciones en listas ,borras la primera y añades la nueva
+//a la 5 se borra
+//a la 4 se comprueba y se borra
+//a la 4 se borra sin comprobar
 const Square = ({children,updateBoard, index}) =>{
 
   const handleClick=()=>{
@@ -27,14 +30,34 @@ const Square = ({children,updateBoard, index}) =>{
 
 
 function App() {
-  
+  const WINNER_COMBOS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+
+  const checkWinner = (boardToCheck)=>{
+    for (const combo of WINNER_COMBOS){
+      const [a,b,c] = combo
+      if (boardToCheck[a] && boardToCheck[a]===boardToCheck[b] && boardToCheck[b]===boardToCheck[c]) {
+        return boardToCheck[a]
+      }
+    }
+  }
   const [board,setBoard] = useState(Array(9).fill(null))
   
   const [turn,setTurn] = useState(TURNS.x)
 
+  const [winner,setWinner] = useState(null)
+
 
   const updateBoard = (index) =>{
-    if (board[index]) return
+    if (board[index]||winner) return
 
     const newBoard=[...board]
   
@@ -43,6 +66,11 @@ function App() {
   
     const newTurn = turn===TURNS.x ? TURNS.o : TURNS.x
     setTurn(newTurn)
+
+    const newWinner =checkWinner(newBoard)
+    if (newWinner) setWinner(newWinner)
+
+
   }
 
   return (
